faa3afab76a5c90e23230441a3dd6a68ce2f2c61
insaiyann <67185462+insaiyann@users.noreply.github.com>
2020-07-23 04:21:41 +0530

    Create script1.js

---
+++ js/script1.js
@@ -0,0 +1,32 @@
+var i=0;
+change();
+var check=0;
+function change()
+{
+check=setInterval(changebackground,2000);
+}
+function changebackground()
+{
+i++;
+if(i==1)
+{
+document.getElementById("body").style="background-image:url('../army8.jpg')";
+}
+if(i==2)
+{
+document.getElementById("body").style="background-image:url('../army6.jpg')";
+}
+if(i==3)
+{
+document.getElementById("body").style="background-image:url('../army7.jpg')";
+}
+if(i==4)
+{
+document.getElementById("body").style="background-image:url('../army9.jpg')";
+}
+if(i==5)
+{
+document.getElementById("body").style="background-image:url('../army10.jpg')";
+i=0;
+}
+}
